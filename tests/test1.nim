c3617627d7c3224e6d335a86f1afbb6979e6ae03
Murat Koptur <mkoptur3@gmail.com>
2023-03-04 11:24:10 +0300

    normal test

--- tests/test1.nim
+++ tests/test1.nim
@@ -1,5 +1,6 @@
 import unittest
 import std/math
+import std/stats
 
 include nim_mcmc
 
@@ -9,10 +10,26 @@ proc testFun(x: float): float =
   else:
     return exp(-x)
 
+proc normalPDF(x: float, mu: float, sigma: float): float =
+  return (1 / (sigma * sqrt(2 * PI))) * exp(-0.5 * ((x - mu) / sigma) ^ 2)
+
+proc testNormal(x: float): float =
+  return normalPDF(x, 3.0, 0.7)
+
 suite "MCMC Tests":
   setup:
-    var sampleSize = 10000
-    var sampleTestFun = mcmc(testFun, sampleSize, 2000)
+    var sampleSize = 100000
+    var sampleTestFun = mcmc(testFun, sampleSize, 20000)
+    var sampleTestNormal = mcmc(testNormal, sampleSize, 20000)
 
   test "correct length":
     check(sampleTestFun.len == sampleSize)
+    check(sampleTestNormal.len == sampleSize)
+
+  test "correct statistics":
+    var statistics: RunningStat
+    statistics.push(sampleTestNormal)
+    # Naive implementation, bad results
+    check(abs(statistics.mean() - 3.0) < 0.01)
+    check(abs(statistics.variance() - 0.7) < 1)
+
